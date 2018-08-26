fd459f8b455b6637bf0f6029dedf07dd203102c8
Gabriel Raymondou <gabrielraymondou@macbook-pro-de-gabriel.local>
2018-08-26 11:21:42 +0200

    Create Account Interface finished

--- Student Quiz/Controllers/CreateAccountViewController.swift
+++ Student Quiz/Controllers/CreateAccountViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CreateAccountViewController: UIViewController {
 
     @IBOutlet weak var EmailTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
