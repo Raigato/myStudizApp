fc6caecc75d72752a64793860644cd5d229368af
RoiGoujat <whysoserious_06@live.fr>
2018-09-26 16:42:45 +0200

    Got ride of the warnings of the Community Quiz interface and linked the UI elements to the code

--- Student Quiz/Controllers/CommunityQuizViewController.swift
+++ Student Quiz/Controllers/CommunityQuizViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class CommunityQuizViewController: UIViewController {
 
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var quizTitleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var sampleQuestionLabel1: UILabel!
+    @IBOutlet weak var sampleQuestionLabel2: UILabel!
+    @IBOutlet weak var questionLabel3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func favoriteButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
     }
     
 }
