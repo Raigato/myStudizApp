f1ac19526d6269a59c48706384d5871755ee97ff
RoiGoujat <whysoserious_06@live.fr>
2018-09-26 16:37:10 +0200

    Made Community Quiz interface

---
+++ Student Quiz/Controllers/CommunityQuizViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommunityQuizViewController.swift
+//  Studiz
+//
+//  Created by Gabriel Raymondou on 26/09/2018.
+//  Copyright © 2018 Gabriel Raymondou. All rights reserved.
+//
+
+import UIKit
+
+class CommunityQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
